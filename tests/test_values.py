db3a927dd830a0a440993627c3dbf7a1d87c7aa5
Sacha Beniamine <4601995-sbeniamine@users.noreply.gitlab.com>
2022-08-01 18:50:28 +0200

    Corrects local import + uncommnt all tests

--- tests/test_values.py
+++ tests/test_values.py
@@ -5,7 +5,7 @@ from setmorph import find_exponents, values_per_word, values_table, \
     classify_allomorphy, count_formatives, Formative
 from pathlib import Path
 from hypothesis import given, note, example, settings
-from strategies import exponents_df
+from .strategies import exponents_df
 from itertools import chain
 from collections import defaultdict
 import pandas as pd
@@ -97,45 +97,45 @@ ex_ve_feats = {'A': {frozenset({'b'}), frozenset({'e'})}}
 
 
 class TestValueClassifications(unittest.TestCase):
-    #
-    # @given(exponents_df())
-    # @example((ex_sg_a, sg))
-    # @example((ex_sg_b, sg))
-    # @example((ex_sg_c, sg))
-    # @example((ex_sg_d, sg))
-    # @example((ex_sg_e, sg))
-    # @settings(deadline=None)
-    # def test_count_formatives_inplace(self, args):
-    #     """Tests counting formatives"""
-    #     df, features = args
-    #     exps = find_exponents(df, features)
-    #     values_words = values_per_word(df, exps)
-    #     values = values_table(values_words)
-    #     count_formatives(values)
-    #     note(values)
-    #
-    #     # changed it in place
-    #     self.assertIn("# formatives", list(values.columns))
-    #
-    # @given(exponents_df())
-    # @example((ex_sg_a, sg))
-    # @example((ex_sg_b, sg))
-    # @example((ex_sg_c, sg))
-    # @example((ex_sg_d, sg))
-    # @example((ex_sg_e, sg))
-    # @settings(deadline=None)
-    # def test_count_formatives_positive_integers(self, args):
-    #     """Tests counting formatives"""
-    #     df, features = args
-    #     exps = find_exponents(df, features)
-    #     values_words = values_per_word(df, exps)
-    #     values = values_table(values_words)
-    #     count_formatives(values)
-    #     note(values)
-    #
-    #     self.assertTrue((values["# formatives"].apply(type) == int).all())
-    #     self.assertTrue((values["# formatives"] > 0).all())
-    #
+
+    @given(exponents_df())
+    @example((ex_sg_a, sg))
+    @example((ex_sg_b, sg))
+    @example((ex_sg_c, sg))
+    @example((ex_sg_d, sg))
+    @example((ex_sg_e, sg))
+    @settings(deadline=None)
+    def test_count_formatives_inplace(self, args):
+        """Tests counting formatives"""
+        df, features = args
+        exps = find_exponents(df, features)
+        values_words = values_per_word(df, exps)
+        values = values_table(values_words)
+        count_formatives(values)
+        note(values)
+
+        # changed it in place
+        self.assertIn("# formatives", list(values.columns))
+
+    @given(exponents_df())
+    @example((ex_sg_a, sg))
+    @example((ex_sg_b, sg))
+    @example((ex_sg_c, sg))
+    @example((ex_sg_d, sg))
+    @example((ex_sg_e, sg))
+    @settings(deadline=None)
+    def test_count_formatives_positive_integers(self, args):
+        """Tests counting formatives"""
+        df, features = args
+        exps = find_exponents(df, features)
+        values_words = values_per_word(df, exps)
+        values = values_table(values_words)
+        count_formatives(values)
+        note(values)
+
+        self.assertTrue((values["# formatives"].apply(type) == int).all())
+        self.assertTrue((values["# formatives"] > 0).all())
+
 
     @given(exponents_df())
     @example((ex_sg_a, sg))
@@ -168,77 +168,77 @@ class TestValueClassifications(unittest.TestCase):
                 self.assertTrue(type(f) is Formative)
 
         values['formative'].apply(contains_formatives_triples)
-    #
-    # @given(exponents_df())
-    # @example((ex_sg_a, sg))
-    # @example((ex_sg_b, sg))
-    # @example((ex_sg_c, sg))
-    # @example((ex_sg_d, sg))
-    # @example((ex_sg_e, sg))
-    # @settings(deadline=None)
-    # def test_values_same_value(self, args):
-    #     """Tests that all formatives have the value in their exponent set."""
-    #     df, features = args
-    #     exps = find_exponents(df, features)
-    #     values_words = values_per_word(df, exps)
-    #     res = values_table(values_words)
-    #     note("Allomorphy:\n\t" + str(res))
-    #     vals = exps.set_index(["lexeme", "tier", "slot", "formative"])["exponence"] \
-    #         .apply(lambda v: set(chain(*v))).to_dict()
-    #
-    #     note(exps)
-    #     note(res)
-    #     res["fs"] = res["formatives_by_word"].apply(lambda x: list(chain(*x)))
-    #     res = res.explode("fs")
-    #
-    #     def same_value(row):
-    #         t, s, f = row["fs"]
-    #         recovered_values = vals[(row.lexeme, t, s, f)]
-    #         self.assertIn(row.value, recovered_values)
-    #
-    #     res.apply(same_value, axis=1)
-    #
-    # @given(exponents_df())
-    # @example((ex_ve_1, ex_ve_feats))
-    # @example((ex_ve_2, ex_ve_feats))
-    # @settings(deadline=None)
-    # def test_values_words_occur_in_same_word(self, args):
-    #     """Test that ..."""
-    #     df, features = args
-    #     exps = find_exponents(df, features)
-    #     res = values_per_word(df, exps)
-    #     note(exps)
-    #     note(res)
-    #
-    #     def exp_list(group):
-    #         return set(group[["tier", "slot", "formative"]].apply(tuple, axis=1))
-    #
-    #     # Build a dict of lexeme, cell => list [ {(formative tuple) }, {}]
-    #     words = df.groupby(["lexeme", "cell", "form"]) \
-    #         .apply(exp_list) \
-    #         .groupby(["lexeme", "cell"]) \
-    #         .apply(list) \
-    #         .to_dict()
-    #
-    #     def check_word(row):
-    #         expected = words[(row.lexeme, row.cell)]
-    #         note(expected)
-    #         self.assertTrue(any(set(row.formative) <= e for e in expected))
-    #
-    #     res.apply(check_word, axis=1)
-    #
-    # @given(exponents_df())
-    # @example((ex_ve_1, ex_ve_feats))
-    # @example((ex_ve_2, ex_ve_feats))
-    # @settings(deadline=None)
-    # def test_values_words_unique_words(self, args):
-    #     """Test that there is a single row for any word"""
-    #     df, features = args
-    #     exps = find_exponents(df, features)
-    #     res = values_per_word(df, exps)
-    #     note(res)
-    #     self.assertFalse(res[["lexeme", "cell", "value", "form"]].duplicated().any())
-    #
+
+    @given(exponents_df())
+    @example((ex_sg_a, sg))
+    @example((ex_sg_b, sg))
+    @example((ex_sg_c, sg))
+    @example((ex_sg_d, sg))
+    @example((ex_sg_e, sg))
+    @settings(deadline=None)
+    def test_values_same_value(self, args):
+        """Tests that all formatives have the value in their exponent set."""
+        df, features = args
+        exps = find_exponents(df, features)
+        values_words = values_per_word(df, exps)
+        res = values_table(values_words)
+        note("Allomorphy:\n\t" + str(res))
+        vals = exps.set_index(["lexeme", "tier", "slot", "formative"])["exponence"] \
+            .apply(lambda v: set(chain(*v))).to_dict()
+
+        note(exps)
+        note(res)
+        res["fs"] = res["formatives_by_word"].apply(lambda x: list(chain(*x)))
+        res = res.explode("fs")
+
+        def same_value(row):
+            t, s, f = row["fs"]
+            recovered_values = vals[(row.lexeme, t, s, f)]
+            self.assertIn(row.value, recovered_values)
+
+        res.apply(same_value, axis=1)
+
+    @given(exponents_df())
+    @example((ex_ve_1, ex_ve_feats))
+    @example((ex_ve_2, ex_ve_feats))
+    @settings(deadline=None)
+    def test_values_words_occur_in_same_word(self, args):
+        """Test that ..."""
+        df, features = args
+        exps = find_exponents(df, features)
+        res = values_per_word(df, exps)
+        note(exps)
+        note(res)
+
+        def exp_list(group):
+            return set(group[["tier", "slot", "formative"]].apply(tuple, axis=1))
+
+        # Build a dict of lexeme, cell => list [ {(formative tuple) }, {}]
+        words = df.groupby(["lexeme", "cell", "form"]) \
+            .apply(exp_list) \
+            .groupby(["lexeme", "cell"]) \
+            .apply(list) \
+            .to_dict()
+
+        def check_word(row):
+            expected = words[(row.lexeme, row.cell)]
+            note(expected)
+            self.assertTrue(any(set(row.formative) <= e for e in expected))
+
+        res.apply(check_word, axis=1)
+
+    @given(exponents_df())
+    @example((ex_ve_1, ex_ve_feats))
+    @example((ex_ve_2, ex_ve_feats))
+    @settings(deadline=None)
+    def test_values_words_unique_words(self, args):
+        """Test that there is a single row for any word"""
+        df, features = args
+        exps = find_exponents(df, features)
+        res = values_per_word(df, exps)
+        note(res)
+        self.assertFalse(res[["lexeme", "cell", "value", "form"]].duplicated().any())
+
 
 
 if __name__ == '__main__':
